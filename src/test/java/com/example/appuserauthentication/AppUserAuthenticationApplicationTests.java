3e91e99d091df18aca9c84a7cd672248d94eb7a7
iulian-stoicescu <iulianstoicescu16@gmail.com>
2021-08-22 15:58:25 +0300

    Initial commit

---
+++ src/test/java/com/example/appuserauthentication/AppUserAuthenticationApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.appuserauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUserAuthenticationApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
